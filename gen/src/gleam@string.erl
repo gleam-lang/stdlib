c9cfb7ef63dcf82e3054d31f9702a835287a4d9e
greggreg <greg@greggreg.org>
2020-04-16 08:50:24 -0600

    Implementing String.contains and String.repeat

--- gen/src/gleam@string.erl
+++ gen/src/gleam@string.erl
@@ -1,7 +1,7 @@
 -module(gleam@string).
 -compile(no_auto_import).
 
--export([is_empty/1, length/1, reverse/1, replace/3, lowercase/1, uppercase/1, compare/2, split/2, append/2, concat/1, join/2]).
+-export([is_empty/1, length/1, reverse/1, replace/3, lowercase/1, uppercase/1, compare/2, contains/2, split/2, append/2, concat/1, repeat/2, join/2]).
 
 is_empty(Str) ->
     Str =:= <<"">>.
@@ -26,6 +26,12 @@ uppercase(A) ->
 compare(A, B) ->
     gleam_stdlib:compare_strings(A, B).
 
+erl_contains(A, B) ->
+    gleam_stdlib:string_contains(A, B).
+
+contains(Haystack, Needle) ->
+    erl_contains(Haystack, Needle).
+
 split(X, Substring) ->
     gleam@list:map(
         gleam@iodata:split(gleam@iodata:new(X), Substring),
@@ -39,6 +45,18 @@ append(First, Second) ->
 
 concat(Strings) ->
     gleam@iodata:to_string(gleam@iodata:from_strings(Strings)).
+
+repeat_help(Chunk, Result, Repeats) ->
+    case Repeats =< 0 of
+        true ->
+            concat(Result);
+
+        false ->
+            repeat_help(Chunk, [Chunk | Result], Repeats - 1)
+    end.
+
+repeat(String, Times) ->
+    repeat_help(String, [<<"">>], Times).
 
 join(Strings, Separator) ->
     gleam@iodata:to_string(
--- gen/test/gleam@string_test.erl
+++ gen/test/gleam@string_test.erl
@@ -1,7 +1,7 @@
 -module(gleam@string_test).
 -compile(no_auto_import).
 
--export([length_test/0, lowercase_test/0, uppercase_test/0, reverse_test/0, split_test/0, replace_test/0, append_test/0, compare_test/0, concat_test/0, join_test/0]).
+-export([length_test/0, lowercase_test/0, uppercase_test/0, reverse_test/0, split_test/0, replace_test/0, append_test/0, repeat_test/0, compare_test/0, contains_test/0, concat_test/0, join_test/0]).
 
 length_test() ->
     gleam@should:equal(gleam@string:length(<<"ß↑e̊">>), 3),
@@ -39,12 +39,25 @@ append_test() ->
         <<"Test Me">>
     ).
 
+repeat_test() ->
+    gleam@should:equal(gleam@string:repeat(<<"hi">>, 3), <<"hihihi">>),
+    gleam@should:equal(gleam@string:repeat(<<"hi">>, 0), <<"">>),
+    gleam@should:equal(gleam@string:repeat(<<"hi">>, -1), <<"">>).
+
 compare_test() ->
     gleam@should:equal(gleam@string:compare(<<"">>, <<"">>), eq),
     gleam@should:equal(gleam@string:compare(<<"a">>, <<"">>), gt),
     gleam@should:equal(gleam@string:compare(<<"a">>, <<"A">>), gt),
     gleam@should:equal(gleam@string:compare(<<"A">>, <<"B">>), lt),
     gleam@should:equal(gleam@string:compare(<<"t">>, <<"ABC">>), gt).
+
+contains_test() ->
+    gleam@should:equal(gleam@string:contains(<<"gleam">>, <<"ea">>), true),
+    gleam@should:equal(gleam@string:contains(<<"gleam">>, <<"x">>), false),
+    gleam@should:equal(
+        gleam@string:contains(<<"bellwether">>, <<"bell">>),
+        true
+    ).
 
 concat_test() ->
     gleam@should:equal(
