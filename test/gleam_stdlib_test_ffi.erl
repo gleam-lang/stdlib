f8c53a55fa7ada91b42c5710e2671dc019a39a5a
inoas <mail@inoas.com>
2022-08-10 15:14:52 +0200

    //erl[1,2|3] output for inspecting improper Erlang lists

--- test/gleam_stdlib_test_ffi.erl
+++ test/gleam_stdlib_test_ffi.erl
@@ -2,7 +2,7 @@
 
 -export([
     main/0, should_equal/2, should_not_equal/2, should_be_ok/1,
-    should_be_error/1, improper_list_append/2
+    should_be_error/1, improper_list_append/3
 ]).
 
 -include_lib("eunit/include/eunit.hrl").
@@ -26,18 +26,18 @@ filepath_to_module(Path0) ->
     Path5 = list_to_binary(Path4),
     binary_to_atom(Path5).
 
-should_equal(Actual, Expected) -> 
+should_equal(Actual, Expected) ->
     ?assertEqual(Expected, Actual),
     nil.
-should_not_equal(Actual, Expected) -> 
+should_not_equal(Actual, Expected) ->
     ?assertNotEqual(Expected, Actual),
     nil.
-should_be_ok(A) -> 
+should_be_ok(A) ->
     ?assertMatch({ok, _}, A),
     nil.
-should_be_error(A) -> 
+should_be_error(A) ->
     ?assertMatch({error, _}, A),
     nil.
 
-improper_list_append(X, Y) ->
-    [X | Y].
+improper_list_append(X, Y, Z) ->
+    [X, Y | Z].
