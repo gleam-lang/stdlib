db350eaf0eb4cefe86f19e553b5637bce442c3c2
brettkolodny <brettkolodny@gmail.com>
2023-02-17 10:05:58 -0500

    fix: deno println to match node implementation

--- src/gleam_stdlib.mjs
+++ src/gleam_stdlib.mjs
@@ -275,6 +275,8 @@ export function bit_string_to_string(bit_string) {
 export function print(string) {
   if (typeof process === "object") {
     process.stdout.write(string); // We can write without a trailing newline
+  } else if (typeof Deno === "object") {
+    Deno.stdout.writeSync(new TextEncoder().encode(string));
   } else {
     console.log(string); // We're in a browser. Newlines are mandated
   }
