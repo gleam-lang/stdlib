014b1f69694abbc8c9f6c91712dad05440079a60
Danny Martini <danny@wunderflats.com>
2023-04-14 23:37:58 +0200

    update erlang implementation

--- src/gleam_stdlib.erl
+++ src/gleam_stdlib.erl
@@ -41,7 +41,7 @@ decode_error_msg(Expected, Data) when is_binary(Expected) ->
 decode_error(Expected, Got) when is_binary(Expected) andalso is_binary(Got) ->
     {error, [{decode_error, Expected, Got, []}]}.
 
-classify_dynamic(X) when is_atom(X) -> <<"Atom">>;
+classify_dynamic(X) when is_atom(X) -> <<"Nil">>;
 classify_dynamic(X) when is_binary(X) -> <<"String">>;
 classify_dynamic(X) when is_bitstring(X) -> <<"BitString">>;
 classify_dynamic(X) when is_integer(X) -> <<"Int">>;
